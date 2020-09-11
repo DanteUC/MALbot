ae213eb2440e15054a210624c237f9041c05b1d6
Dante Umpherville-Choy <45216212+DanteUC@users.noreply.github.com>
2020-09-10 18:09:01 -0700

    Update index.js
    Fixed multi-word genres, added missing ping and changed pog so that it will send without command prefix

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ client.once('ready', () => {
 function emoji(id){
     return client.emojis.cache.get(id).toString();
 }
-//TODO: make a helper function that initializes a temp list, selects an anime that was rated high enough from the list, and sends the selection to chat
-//to replace the current "send the first item" issue
 
 
 let genreList ={
@@ -34,26 +32,26 @@ let genreList ={
     "horror":14,
     "kids":15,
     "magic":16,
-    "martial arts":17,
+    "martial_arts":17,
     "mecha":18,
     "music":19,
     "parody":20,
     "samurai":21,
     "romance":22,
     "school":23,
-    "sci fi":24,
+    "sci_fi":24,
     "shoujo":25,
-    "shoujo ai":26,
+    "shoujo_ai":26,
     "shounen":27,
-    "shounen ai":28,
+    "shounen_ai":28,
     "space":29,
     "sports":30,
-    "super power":31,
+    "super_power":31,
     "vampire":32,
     "yaoi":33,
     "yuri":34,
     "harem":35,
-    "slice of life":36,
+    "slice_of_life":36,
     "supernatural":37,
     "military":38,
     "police":39,
@@ -64,6 +62,12 @@ let genreList ={
 }
 
 client.on('message', message =>{
+    if(message.content.includes("?")){
+        message.channel.send(emoji ("753769067034312854") );
+    }
+    if(message.content.includes(("pog"))){
+        message.channel.send(emoji ("643065894138019840") );
+    }
     if(!message.content.startsWith(prefix)||message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
@@ -95,10 +99,8 @@ client.on('message', message =>{
             listString+= "\n" + genre;
         }
         message.channel.send(listString);
-    }else if(command === 'pog'){
-        message.channel.send(emoji ("643065894138019840") );
     }else if (command === 'help'){
-        message.channel.send('!pog: pog in the chat\n![genre]: get an anime recommendation of the given genre\n!list: list the genres supported on MAL')
+        message.channel.send('&[genre]: get an anime recommendation of the given genre\n&list: list the genres supported on MAL')
     }
 });
 
